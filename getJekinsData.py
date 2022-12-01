fa7d476207e470c7dba921bac17e4019abefdc8f
israellopezlara09 <119519049+israellopezlara09@users.noreply.github.com>
2022-12-01 15:52:49 -0500

    Update getJekinsData.py

--- getJekinsData.py
+++ getJekinsData.py
@@ -6,7 +6,7 @@ import json
 URL = "https://jenkins-66e2ed6e-af76-40f4-a86b-b7cde6a06963.live.alta3.com/api/json?pretty=true"
 
 # sending get request and saving the response as response object
-r = requests.get(url = URL, auth = ('jarvis', 'alta3'),verify=False)
+r = requests.get(url = URL, auth = ('jarvis', '118183c729b6668877fc71c9d269ac7b13'),verify=False)
   
 # extracting data in json format
 data = json.loads(r.text)
