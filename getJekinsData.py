3546696a32c23d539384128b98c8263b2dc5525d
israellopezlara09 <119519049+israellopezlara09@users.noreply.github.com>
2022-12-01 15:19:19 -0500

    Update getJekinsData.py

--- getJekinsData.py
+++ getJekinsData.py
@@ -6,7 +6,7 @@ import requests
 URL = "http://0.0.0.0:2224/api/python?pretty=true --user jarvis:118183c729b6668877fc71c9d269ac7b13"
 
 # sending get request and saving the response as response object
-r = requests.get(url = URL, params = PARAMS)
+r = requests.get(url = URL)
   
 # extracting data in json format
 data = r.json()
